4d913e724abe295a9affd01522127a09c9c398a2
Samantha <samanthaltl.sfh@gmail.com>
2022-02-15 17:58:29 -0800

    basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="app-header">
+
+        <div className="navigation">
+          <a href="https://www.twitter.com/sammidoesthings"><button>twitter</button></a>
+          <a href="https://www.instagram.com/sammidoesthings"><button>instagram</button></a>
+          <a href="https://www.linkedin.com/in/samanthalittle8436"><button>linkedin</button></a>
+          <a href="https://www.github.com/sammidoesthings"><button>github</button></a>
+          <a href="https://www.youtube.com/channel/UCDPfahiBCMQQLYsccSXhc-A"><button>youtube</button></a>
+        </div>
+
+        <div className="site-title">
+          <h2>sammi does things</h2>
+        </div>
+
+        {/* <div className="home-button-div">
+          <a href="/home"><button className="home-button">sammi home</button></a>
+        </div> */}
+
+        {/* HEADER ENDS HERE */}
+      </div>
+
+      <div className="intro-card">
+
+        <div className="intro-about">
+          <div className="intro-about-text">
+            <h3>Hello, there!</h3>
+            <p>I'm Sammi -- a full stack web developer based out of coffee shops near the beautiful beaches of San Francisco!</p> 
+            <p>I have a passion for designing sites that are functional <span classname="italics">and</span> pretty.</p>
+            <p>~~~ * ~~~</p>
+            <p>I work primarily in React, Next, and Node, but I've recently started learning Python.</p> 
+            <p>You can read about my journey on Medium or scroll down to see my projects!</p>
+          </div>
+        </div>
+
+        <div className="intro-photo">
+          <img src="../assets/profile-photo.jpg" alt="#"/>
+        </div>
+      </div>
+
+      <div className="projects-container">
+        <div class="site-project">
+          <img src="assets/project1.jpeg" alt="#"/>
+          <h2>Project Name</h2>
+        </div>
+        <div class="site-project">
+          <img src="assets/project1.jpeg" alt="#"/>
+          <h2>Project Name</h2>
+        </div>
+      </div>
+      {/* APP CONTAINER ENDS HERE */}
     </div>
   );
 }
